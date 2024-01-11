12c60e4c4682dedb5b2e0ee382107046f5c44325
MxNl <maxnoelscher@posteo.de>
2024-01-11 15:24:33 +0100

    adaptations for challenge

--- R/postprocess_extracted_values.R
+++ R/postprocess_extracted_values.R
@@ -1,10 +1,15 @@
 postprocess_extracted_values <- function(x) {
   x |>
     dplyr::mutate(
-      dplyr::across(dplyr::any_of(c("gwn_recharge")),
+      dplyr::across(dplyr::any_of(c("feat_hydro_gwn_recharge")),
              ~.x |> stringr::str_remove_all(" mm/Jahr") |>
                as.numeric()
              )
       ) |>
-    dplyr::mutate(dplyr::across(dplyr::where(is.character), as.factor))
+    dplyr::mutate(dplyr::across(dplyr::where(is.character), as.factor)) |>
+    dplyr::select(-matches("100km|50km|10km|5km")) |>
+    dplyr::select(-dplyr::num_range("feat_topo_eumohp_sd", 2:9)) |>
+    dplyr::select(-dplyr::num_range("feat_topo_eumohp_dsd", 2:9)) |>
+    dplyr::select(-dplyr::num_range("feat_topo_eumohp_lp", 2:9))
 }
+
